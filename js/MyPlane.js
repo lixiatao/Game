6d70f7b1d98ab44a235b1b3658e5421468d5b900
TaoLixia <1357311395@qq.com>
2017-09-15 13:58:57 +0800

    Update MyPlane.js

--- js/MyPlane.js
+++ js/MyPlane.js
@@ -1,3 +1,4 @@
+'use strict';
 (function(){
 	var MyPlane = window.MyPlane = function(){
 		this.image = game.R['myplane1'];
@@ -7,15 +8,15 @@
 		this.y = game.canvas.height - 130;
 		this.isdie = false;
 
-	}
+	};
 	MyPlane.prototype.render = function(){
 		game.ctx.drawImage(this.image, this.x, this.y);
-	}
+	};
 	MyPlane.prototype.update = function(){
 		// 所占矩形块位置
 		this.my_T = this.y;
 		this.my_R = this.x + this.width;
 		this.my_B = this.y + this.height;
 		this.my_L = this.x;
-	}
+	};
 })();
